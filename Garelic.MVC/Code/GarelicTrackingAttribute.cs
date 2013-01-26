95b361d625b8071caaebab49ac1471b8d488360f
brano <brano@yweb.sk>
2013-01-26 22:50:08 +0100

    Initial project commit
    GarelicTrackingAttribute - log controller`s actions before/after
    execution

---
+++ Garelic.MVC/Code/GarelicTrackingAttribute.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Garelic.MVC
+{
+    public class GarelicTrackingAttribute : ActionFilterAttribute, IActionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var actionDescriptor = filterContext.ActionDescriptor;
+            string controllerName = actionDescriptor.ControllerDescriptor.ControllerName;
+            string actionName = actionDescriptor.ActionName;
+            string timeStamp = string.Format("{0:yyyy-MM-dd HH:mm:ss}", DateTime.Now);
+            string routeId = filterContext.RouteData.Values["id"] != null ? filterContext.RouteData.Values["id"].ToString() : string.Empty;
+
+            StringBuilder message = new StringBuilder();
+            message.Append("Controller=");
+            message.Append(controllerName + "|");
+            message.Append("Action=");
+            message.Append(actionName + "|");
+            
+            if (!string.IsNullOrEmpty(routeId))
+            {
+                message.Append("Route=");
+                message.Append(routeId + "|");
+            }
+
+            message.Append("Date=");
+            message.Append(timeStamp);
+
+            System.Diagnostics.Debug.WriteLine(message.ToString());
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var actionDescriptor = filterContext.ActionDescriptor;
+            string controllerName = actionDescriptor.ControllerDescriptor.ControllerName;
+            string actionName = actionDescriptor.ActionName;
+            string timeStamp = string.Format("{0:yyyy-MM-dd HH:mm:ss}", DateTime.Now);
+            string routeId = filterContext.RouteData.Values["id"] != null ? filterContext.RouteData.Values["id"].ToString() : string.Empty;
+
+            StringBuilder message = new StringBuilder();
+            message.Append("Controller=");
+            message.Append(controllerName + "|");
+            message.Append("Action=");
+            message.Append(actionName + "|");
+
+            if (!string.IsNullOrEmpty(routeId))
+            {
+                message.Append("Route=");
+                message.Append(routeId + "|");
+            }
+
+            message.Append("Date=");
+            message.Append(timeStamp);
+
+            System.Diagnostics.Debug.WriteLine(message.ToString());
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
---
+++ Garelic.MVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Garelic.MVC.Controllers
+{
+    [GarelicTrackingAttribute]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            System.Threading.Thread.Sleep(5000); // wait 5 sec.
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
